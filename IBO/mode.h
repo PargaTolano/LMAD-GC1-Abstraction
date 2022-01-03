696f92e2083479e548ff451bc8e741e11f7f1b9d
Jose Parga Tolano <parga.jose@outlook.com>
2022-01-03 15:23:41 -0700

    quick cleanup of unused projects

--- IBO/mode.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-//define either _WINDOWSx64_MODE or _GLFW3_MODE, but not both
-#define _GLFW3_MODE
--- IBO/utils.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#define unsafe_array_size(x) sizeof(x)/sizeof(x[0])
